28016f9a4a89603b682ffc7bac3d26fcafc85a81
pauloesmelos <pauloempdel@hotmailcom>
2023-11-17 09:20:53 -0300

    added message good day or good night in the home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import FormSearch from './components/FormSearch'
+import React from 'react';
+import FormSearch from './components/FormSearch';
+import {} from 'react-icons';
 
 const Home = () => {// BOM DIA ! BOA NOITE IMPLEMENTAR
   const isDay = () => {
-    const hour = new Date().getUTCHours();
-    console.log(hour);
+    const hour = new Date().getHours();
+    return (hour > 6) && (hour < 18) ? true : false;
   }
-  React.useEffect(() => {
+  const time = () => {
+    const hour = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    return `${hour}:${minutes}`;
+  }
 
-  }, []);
   return (
-    <div className="bg-gray-100 bg-cover bg-center bg-no-repeat
-    h-screen w-full flex justify-center items-center">
+    <main>
+      <div className="bg-gray-100 relative g-gray-100 w-full py-5 z-30 flex flex-col sm:flex-row
+      sm:gap-5 justify-center items-center">
+        <h2 className="text-slate-500 text-2xl sm:text-3xl font-serif text-center font-bold before:inline-block 
+        before:-z-20 relative before:w-3 before:h-3 before:bg-yellow-300 before:mr-[-12px]">
+          {isDay ? "Bom dia" : "Boa noite"}
+        </h2>
+        <div>
+          <span className="text-gray-400 text-[.8rem] sm:text-sm">{time()} da manhã</span>
+        </div>
+      </div>
+      <div className="bg-gray-100 bg-cover bg-center bg-no-repeat
+      h-screen w-full flex justify-center items-center">
         <FormSearch />
-    </div>
+      </div>
+    </main>
   )
 }
 
-export default Home
+export default Home;
